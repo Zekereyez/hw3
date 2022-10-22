91675a79af01892b01b4208f862963d18a517dbc
Ezequiel Reyes <reyeseze@usc.edu>
2022-10-21 18:40:24 -0700

    logic section

--- logicsim/event.h
+++ logicsim/event.h
@@ -10,7 +10,17 @@ struct Event
 };
 
 typedef struct EventLess {
-        //write the operator() required to make this a functor that compares Events by time
+    //write the operator() required to make this a functor that compares Events by time
+    bool operator()(Event* event1, Event* event2) {
+        // events are sorted by time and we need to check if the 
+        // times of the events are less than ie min heap
+        if (event1->time < event2->time) {
+            return true;
+        }
+        // if times are equal doesnt matter which order 
+        // if greater time then returns false
+        return false;
+    }
 } EventLess;
 	
 #endif
