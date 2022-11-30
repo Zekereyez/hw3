21f738f22b53e7e439e462a46141f10fc5a1b555
Ezequiel Reyes <reyeseze@usc.edu>
2022-11-29 18:15:56 -0800

    100% passing code

--- llrec.cpp
+++ llrec.cpp
@@ -4,7 +4,7 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
-void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot) {
     // recursion baabyyyyy
     // should be an easy and fast implementation 
     // need helper function for this 
@@ -13,15 +13,29 @@ void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
         larger = nullptr;
         return;
     }
+    // suggestive case where we know we are at the penultimate node
+    if (head->next == nullptr) {
+      if (head->val <= pivot) {
+        smaller = head;
+        larger = nullptr;
+      }
+      else {
+        smaller = nullptr;
+        larger = head;
+      }
+      head = nullptr;
+    }
     // the value must be equal or less than the pivot in order to be 
     // in the smaller node list
-    if (head->val <= pivot) {
+    else if (head->val <= pivot) {
         smaller = head;
-        llpivot(head->next, smaller->next, larger, pivot);
+        head = head->next;
+        llpivot(head, smaller->next, larger, pivot);
     }
     // if strictly bigger then we assign into the larger node list
     else {
         larger = head;
-        llpivot(head->next, smaller, larger->next, pivot);
+        head = head->next;
+        llpivot(head, smaller, larger->next, pivot);
     }
 }
